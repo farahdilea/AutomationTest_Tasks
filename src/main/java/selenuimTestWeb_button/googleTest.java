27bac0186fe8c9ceec7cac08ff5ae7010dcdeae3
farahdilea <152026803+farahdilea@users.noreply.github.com>
2024-05-23 10:44:20 +0300

    Update googleTest.java

--- src/main/java/selenuimTestWeb_button/googleTest.java
+++ src/main/java/selenuimTestWeb_button/googleTest.java
@@ -10,18 +10,14 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class googleTest {
  
-	public static void main(String[] args) throws InterruptedException {
+public static void main(String[] args) {
 		// TODO Auto-generated method stub
 WebDriver driver = new ChromeDriver();
 driver.get("https://www.google.com");
 driver.manage().window().maximize();
-Thread.sleep(1000);
 driver.findElement(By.name("q")).sendKeys("Selenium Tutorials");
-Thread.sleep(1000);
-
 WebElement searchButton = driver.findElement(By.name("btnK"));
 searchButton.click();
-Thread.sleep(1000);
 driver.close();	
 }	
 }
